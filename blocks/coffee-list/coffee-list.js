aa47a1e9c9a534a8b903600529459257aea564f3
AntonniSMoraes <antonnimoraes@gmail.com>
2026-08-27 20:51:50 -0300

    feat: mudança do container para receber diversos cards

--- blocks/coffee-list/coffee-list.js
+++ blocks/coffee-list/coffee-list.js
@@ -1,19 +1,17 @@
 export default async function decorate(block) {
-  const links = [...block.querySelectorAll('a')];
-  console.log("links: ", links);
+  const rows = [...block.children];
 
-  block.innerHTML = '';
-
-  if (links.length === 0) {
-    block.innerHTML = '<p>Selecione um ou mais cafés no painel.</p>';
+  if (rows.length === 0) {
+    block.innerHTML = '<p>☕ Selecione a lista no painel e adicione os Cafés.</p>';
     return;
   }
 
-  const container = document.createElement('div');
-  container.className = 'coffee-cards-container';
-  block.append(container);
+  block.classList.add('coffee-cards-container');
 
-  const fetchPromises = links.map(async (link) => {
+  const fetchPromises = rows.map(async (row) => {
+    const link = row.querySelector('a');
+    if (!link) return; 
+
     let fragmentPath = link.getAttribute('href');
     if (fragmentPath.endsWith('.html')) {
       fragmentPath = fragmentPath.replace(/\.html$/, '');
@@ -23,12 +21,10 @@ export default async function decorate(block) {
       const response = await fetch(`${fragmentPath}.infinity.json`);
       if (!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
       
-      
       const data = await response.json();
       const cfData = data['jcr:content']?.data?.master;
-      console.log("resposta: ", cfData);
 
-      if (!cfData) return null;
+      if (!cfData) return;
 
       let nomeDoProdutor = 'Produtor desconhecido';
       if (cfData.produtorRef && typeof cfData.produtorRef === 'string') {
@@ -45,29 +41,18 @@ export default async function decorate(block) {
         }
       }
 
-      const card = document.createElement('div');
-      card.className = 'coffee-card';
-      
-      card.innerHTML = `
+      row.className = 'coffee-card';
+      row.innerHTML = `
         <img src="${cfData.fotoCafe}" alt="${cfData.nomeCafe}" style="max-width: 100%; border-radius: 8px;" />
         <h3>${cfData.nomeCafe}</h3>
         <p>${cfData.origemCafe}</p>
         <p><strong>Produtor:</strong> ${nomeDoProdutor}</p>
       `;
 
-      return card;
-
     } catch (error) {
       console.error(`Erro ao carregar o fragmento: ${fragmentPath}`, error);
-      return null;
     }
   });
 
-  const cardsProntos = await Promise.all(fetchPromises);
-
-  cardsProntos.forEach(card => {
-    if (card) {
-      container.append(card);
-    }
-  });
+  await Promise.all(fetchPromises);
 }
